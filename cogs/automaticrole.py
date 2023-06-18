d62bacda3aa3c6329557f032adf6cd2d3536e077
kiwigab <77127009+kiwigab@users.noreply.github.com>
2023-06-18 13:05:13 +0300

    Fixed a bug!

--- cogs/automaticrole.py
+++ cogs/automaticrole.py
@@ -16,11 +16,12 @@ class Automaticrole(commands.Cog):
         guild_data = await self.database.get_guild_data(member.guild.id)
 
         human_role_id = guild_data[0]["human_role_id"]
-        if human_role_id:
+        bot_role_id = guild_data[0]["bot_role_id"]
+
+        if human_role_id or not bot_role_id:
             role = member.guild.get_role(human_role_id)
             await member.add_roles(role)
 
-        bot_role_id = guild_data[0]["bot_role_id"]
         if bot_role_id and member.bot:
             role = member.guild.get_role(bot_role_id)
             await member.add_roles(role)
